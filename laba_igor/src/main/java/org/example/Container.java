22730481c0afc97c229e03ee6786eedaf5664f63
SpitsyinIgor <1overflash1@gmail.com>
2024-10-24 00:54:51 +0300

    Добавление контейнеров для классов Container и Node, Update Container.java
    Node служит для хранения значений, ссылок на предыдущую и будущую ноду. Container предназначен для хранения значений типа int.

---
+++ laba_igor/src/main/java/org/example/Container.java
@@ -0,0 +1,23 @@
+public class Container {
+    private Node head;
+
+    static class Node {
+        int item;
+        Node next;
+        Node previous;
+
+        Node() {
+        }
+
+        Node(int item) {
+            this.item = item;
+        }
+    }
+
+    Container() {
+    }
+
+    Container(int... items) {
+        for (int item : items) add(item);
+    }
+}
